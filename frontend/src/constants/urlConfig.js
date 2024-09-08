6fc26f65add5071dca3e7b654846cf9850a92a24
romanyn36 <romanyyy88@gmail.com>
2024-09-08 03:58:44 +0300

    set new host

--- frontend/src/constants/urlConfig.js
+++ frontend/src/constants/urlConfig.js
@@ -1,4 +1,4 @@
-// export const REACT_APP_API_URL='https://django-shopping-ddf7cf247301.herokuapp.com'
-// export const REACT_APP_API_URL='https://django-shopping-ddf7cf247301.herokuapp.com'
-export const REACT_APP_API_URL='http://127.0.0.1:8000'
-export const MEDIA_BASE_URL="http://127.0.0.1:8000"
+export const REACT_APP_API_URL='https://django-shopping.osc-fr1.scalingo.io'
+export const MEDIA_BASE_URL='https://django-shopping.osc-fr1.scalingo.io'
+// export const REACT_APP_API_URL='http://127.0.0.1:8000'
+// export const MEDIA_BASE_URL="http://127.0.0.1:8000"
